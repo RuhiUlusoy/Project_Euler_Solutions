cea02da8a6f9765231bdf8fe18180aae18d57f58
RuhiUlusoy <140948847+RuhiUlusoy@users.noreply.github.com>
2023-07-30 13:25:10 +0300

    Add files via upload

---
+++ PrimeNumbers.kt
@@ -0,0 +1,27 @@
+fun main(args : Array<String>) {
+
+    fun checkPrime(num:Int): Boolean{
+        var control = true
+
+        for (i in 2..num / 2) {
+            if (num % i == 0) {
+                control = false
+                break
+            }
+        }
+
+        if (control == true && (num > 1)) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+   var x = 20
+    for (i in 2..x){
+        if (checkPrime(i)){
+            print("$i ")
+        }
+    }
+
+}
