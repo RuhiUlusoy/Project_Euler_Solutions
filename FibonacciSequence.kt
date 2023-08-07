e39de7434be9935ca3076b2fd9ba1e9394beda86
RuhiUlusoy <140948847+RuhiUlusoy@users.noreply.github.com>
2023-08-07 21:57:33 +0300

    Fibonacci Sequence
    Prints the first 20 Fibonacci numbers

---
+++ FibonacciSequence.kt
@@ -0,0 +1,15 @@
+fun main(args : Array<String>) {
+    var sum0 = 1
+    var sum1 = 1
+    for(i in 1..20){
+        if (i == 1 || i == 2){
+            print("F$i = $sum0 ")
+        }
+        else{
+          var sum2 = sum0 + sum1
+          print("F$i = $sum2 ")
+          sum0 = sum1
+          sum1 = sum2
+        }
+    }
+}
