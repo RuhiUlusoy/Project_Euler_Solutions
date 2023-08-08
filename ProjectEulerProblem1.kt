2c10038ce8395197da838e0c9e4f6e8a2d7e9295
RuhiUlusoy <140948847+RuhiUlusoy@users.noreply.github.com>
2023-08-08 21:50:17 +0300

    ProjectEulerProblem1.kt

--- ProjectEulerProblem1.kt
+++ ProjectEulerProblem1.kt
@@ -1,11 +1,7 @@
 fun main(args: Array<String>) {
     var sum = 0
     for (i in 3..999){
-        if (i%3==0) sum +=i
-
-        if (i%5==0) sum +=i
-
-        if(i%15==0) sum -=i
+        if (i%3==0 || i%5==0) sum +=i
     }
     print(sum)
 }
