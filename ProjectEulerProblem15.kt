1bb327c5b8c760fe5806c78be7393e96f3c77c0d
RuhiUlusoy <140948847+RuhiUlusoy@users.noreply.github.com>
2023-08-15 11:52:02 +0300

    Project Euler Problem 15 Solution With Kotlin

--- ProjectEulerProblem15.kt
+++ ProjectEulerProblem15.kt
@@ -3,7 +3,7 @@ fun main(args: Array<String>) {
 
     // Math Solution = (2*num)!/(num!*num!)
 
-    fun solution(num:Long): Long{
+    fun solution1(num:Long): Long{
         var result = 1L
         for(i in 1..num){
             result = result * (num + i) / i
@@ -12,6 +12,38 @@ fun main(args: Array<String>) {
         return result
     }
 
-    println(solution(20L))
+    println(solution1(20L))
+
+   
+    //Second Solution
+
+    fun solution2(num:Int): Int{
+        val rows = num
+        val cols = num
+
+        val arr : Array<IntArray> = Array(rows) { IntArray(cols) }
+
+        for (i in 0..num-1) {
+            arr[0][i] = 1
+        }
+
+        for (m in 1..num-1) {
+            for (n in 0..num-1) {
+                if (n == 0) {
+                    arr[m][n] = 1
+                } else {
+                    arr[m][n] = arr[m][n - 1] + arr[m - 1][n]
+                }
+            }
+        }
+
+        for (row in arr) {
+            println(row.contentToString())
+        }
+
+        return arr[num-1][num-1]
+    }
+
+    println(solution2(4))
 
   }
